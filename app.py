962f4412fdf71d4c5ca218f4e4d1af3ec0685daa
masterjedi-ltd <info@masterjedi.pl>
2026-01-29 21:56:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'API działa! Użyj /trends?keyword=bitcoin lub /trending'
+    return 'Worgz! /trends?keyword=bitcoin lub /trending'
 
 @app.route('/trending')
 def get_trending():
